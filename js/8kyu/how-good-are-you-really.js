50a78e1e56abf6a770c054076a205a047f7f8dab
dariantonova <186691008+dariantonova@users.noreply.github.com>
2024-11-06 12:22:26 +0300

    How good are you really

---
+++ js/8kyu/how-good-are-you-really.js
@@ -0,0 +1,17 @@
+// ❓ DESCRIPTION
+// There was a test in your class and you passed it. Congratulations!
+//
+// But you're an ambitious person. You want to know if you're better than the average student in your class.
+//
+// You receive an array with your peers' test scores. Now calculate the average and compare your score!
+//
+// Return true if you're better, else false!
+//
+// Note:
+//     Your points are not included in the array of your class's points.
+
+// ✅ SOLUTION
+function betterThanAverage(classPoints, yourPoints) {
+    const avg = classPoints.reduce((sum, a) => sum + a, 0) / classPoints.length;
+    return yourPoints > avg;
+}
