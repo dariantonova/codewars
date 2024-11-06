12b357d1fc688469ff9e66595d0c3c01d2113d06
dariantonova <186691008+dariantonova@users.noreply.github.com>
2024-11-06 14:02:33 +0300

    Are You Playing Banjo

---
+++ js/8kyu/are-you-playing-banjo.js
@@ -0,0 +1,14 @@
+// ❓ DESCRIPTION
+// Create a function which answers the question "Are you playing banjo?".
+//     If your name starts with the letter "R" or lower case "r", you are playing banjo!
+//
+//     The function takes a name as its only argument, and returns one of the following strings:
+//
+//     name + " plays banjo"
+// name + " does not play banjo"
+// Names given are always valid strings.
+
+// ✅ SOLUTION
+function areYouPlayingBanjo(name) {
+    return name.at(0).match(/r/i) ? `${name} plays banjo` : `${name} does not play banjo`;
+}
